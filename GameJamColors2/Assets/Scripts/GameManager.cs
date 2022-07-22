5eb025fe435176678fd192770d399e532d37dd99
oscar <osgarc05@ucm.es>
2022-07-22 19:08:56 +0200

    Managers añadidos

---
+++ GameJamColors2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    public float mainVolSlider = 0.5f,
+                 SFXVolSlider = 0.5f,
+                 musicVolSlider = 0.5f;
+    public bool gameIsPaused, needToPause, needToResume;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random.InitState((int)DateTime.Now.Ticks);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MainSliderState(float volume)
+    {
+        mainVolSlider = volume;
+    }
+    public void MusicSliderState(float volume)
+    {
+        musicVolSlider = volume;
+    }
+    public void SFXSliderState(float volume)
+    {
+        SFXVolSlider = volume;
+    }
+}
---
+++ GameJamColors2/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject optionsFirstButton = null, optionsUI = null, mainUI = null, loadingText = null; // Referencian el menú de opciones y el botón seleccionado al abrirlo
+
+    private void Start() // Inicia la música del menú
+    {
+        AudioManager.instance.Play(AudioManager.ESounds.tintontin1);
+    }
+
+    public void Play() // Para la música del menú y carga la primera escena
+    {
+        loadingText.SetActive(true);
+        mainUI.SetActive(false);
+        Cursor.visible = false;
+        AudioManager.instance.Play(AudioManager.ESounds.tintontin1);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit() // Cierra el juego
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void Options() // Abre el menú de opciones
+    {
+        optionsUI.SetActive(true);
+        mainUI.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+}
