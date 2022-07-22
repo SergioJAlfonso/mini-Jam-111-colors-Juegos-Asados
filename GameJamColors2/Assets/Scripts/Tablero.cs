5fb52ca419a6cc0c6b0d5179333fc6492f205bd7
oscar <osgarc05@ucm.es>
2022-07-22 13:57:08 +0200

    Tablero con margenes

--- GameJamColors2/Assets/Scripts/Tablero.cs
+++ GameJamColors2/Assets/Scripts/Tablero.cs
@@ -8,21 +8,24 @@ public class Tablero : MonoBehaviour
     GameObject tile;
     [SerializeField]
     int x, y;
+    [SerializeField]
+    float margin;
     // Start is called before the first frame update
     void Start()
     {
         Vector3 pos = new Vector2(0,0);
         Vector3 sc = tile.transform.localScale;
-        pos.x = sc.x * (x/2.0f);
+        //pos.x = -sc.x * (x/2.0f) - margin*((x-1)/2.0f);
         for(int i=0; i<x; i++)
         {
-            pos.z = sc.z * (y/2.0f);
+            //pos.z = -sc.z * (y/2.0f) - margin * ((y - 1) / 2.0f);
+            pos.z = 0;
             for(int j=0; j<y; j++)
             {
-                pos.z += sc.z;
                 Instantiate(tile, pos, Quaternion.identity, this.transform);
+                pos.z += sc.z + margin;
             }
-            pos.x += sc.x;
+            pos.x += sc.x + margin;
         }
     }
 
